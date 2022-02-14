3ece770fa10754789706f82a54ded88571e05fa3
bodrulamin <bodrulaminiu@gmail.com>
2022-02-14 13:00:33 +0600

    m

--- CIS-Frontend/src/app/components/subscription/subscription.component.ts
+++ CIS-Frontend/src/app/components/subscription/subscription.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from 'src/app/services/category.service';
+import {ToastService} from "../../services/toast.service";
 
 @Component({
   selector: 'app-subscription',
@@ -9,7 +10,8 @@ import { CategoryService } from 'src/app/services/category.service';
 export class SubscriptionComponent implements OnInit {
 
   constructor(
-    private categoryService: CategoryService
+    private categoryService: CategoryService,
+    private toast: ToastService
   ) { }
 
   categories:any
@@ -18,6 +20,14 @@ export class SubscriptionComponent implements OnInit {
     this.categoryService.getAll().subscribe(res=>{
       this.categories = res.data.category
     })
+  }
+
+  subscribe(catname: string){
+    console.log(catname + 'subscribe')
+    this.toast.success('Subscribed to ' + catname)
+  }
+  unsubscribe(catname: string){
+    this.toast.success('Unubscribed to ' + catname)
   }
 
 }
